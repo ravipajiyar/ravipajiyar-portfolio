3da2da42febe94ee869c33d66d28e385da6aca31
ravi-pajiyar <pajiyarravi20011@gmail.com>
2026-06-21 21:34:41 +0545

    config file changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-
-  basePath: isGithubPages ? '/ravipajiyar-portfolio' : '',
-  assetPrefix: isGithubPages ? '/ravipajiyar-portfolio/' : '',
 
   eslint: {
     ignoreDuringBuilds: true,
